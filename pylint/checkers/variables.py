0d60a05973830773abbefcb408688c2fce9837da
Jacob Walls <jacobtylerwalls@gmail.com>
2023-05-16 15:10:25 -0400

    Cause unwanted changes

--- pylint/checkers/variables.py
+++ pylint/checkers/variables.py
@@ -881,31 +881,31 @@ scope_type : {self._atomic.scope_type}
                 for else_statement in closest_try_except.orelse
             )
 
-            if try_block_returns or else_block_returns or else_block_exits:
-                # Exception: if this node is in the final block of the other_node_statement,
-                # it will execute before returning. Assume the except statements are uncertain.
-                if (
-                    isinstance(node_statement.parent, nodes.TryFinally)
-                    and node_statement in node_statement.parent.finalbody
-                    and closest_try_except.parent.parent_of(node_statement)
-                ):
-                    uncertain_nodes.append(other_node)
-                # Or the node_statement is in the else block of the relevant TryExcept
-                elif (
-                    isinstance(node_statement.parent, nodes.TryExcept)
-                    and node_statement in node_statement.parent.orelse
-                    and closest_try_except.parent.parent_of(node_statement)
-                ):
-                    uncertain_nodes.append(other_node)
-                # Assume the except blocks execute, so long as each handler
-                # defines the name, raises, or returns.
-                elif all(
-                    NamesConsumer._defines_name_raises_or_returns_recursive(
-                        node.name, handler
-                    )
-                    for handler in closest_try_except.handlers
-                ):
-                    continue
+            # if try_block_returns or else_block_returns or else_block_exits:
+            #     # Exception: if this node is in the final block of the other_node_statement,
+            #     # it will execute before returning. Assume the except statements are uncertain.
+            #     if (
+            #         isinstance(node_statement.parent, nodes.TryFinally)
+            #         and node_statement in node_statement.parent.finalbody
+            #         and closest_try_except.parent.parent_of(node_statement)
+            #     ):
+            #         uncertain_nodes.append(other_node)
+            #     # Or the node_statement is in the else block of the relevant TryExcept
+            #     elif (
+            #         isinstance(node_statement.parent, nodes.TryExcept)
+            #         and node_statement in node_statement.parent.orelse
+            #         and closest_try_except.parent.parent_of(node_statement)
+            #     ):
+            #         uncertain_nodes.append(other_node)
+            #     # Assume the except blocks execute, so long as each handler
+            #     # defines the name, raises, or returns.
+            #     elif all(
+            #         NamesConsumer._defines_name_raises_or_returns_recursive(
+            #             node.name, handler
+            #         )
+            #         for handler in closest_try_except.handlers
+            #     ):
+            #         continue
 
             if NamesConsumer._check_loop_finishes_via_except(node, closest_try_except):
                 continue
